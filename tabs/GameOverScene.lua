baad9f9d2ea78598d2a8cf70fec7f1733c6d7be1
Mr-Coxall at codea-scm <Mr-Coxall@codea-scm.aws.mapote.com>
2016-01-10 22:48:43 -0500

    initial commit

---
+++ tabs/GameOverScene.lua
@@ -0,0 +1,14 @@
+GameOverScene = class()
+
+function GameOverScene:init(x)
+    -- you can accept and set parameters here
+    self.x = x
+end
+
+function GameOverScene:draw()
+    -- Codea does not automatically call this method
+end
+
+function GameOverScene:touched(touch)
+    -- Codea does not automatically call this method
+end
